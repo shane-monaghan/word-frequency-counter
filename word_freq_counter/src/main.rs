5bcc2973d9e98f4c77c05d7c6d58824f953639b2
shane-monaghan <135287482+shane-monaghan@users.noreply.github.com>
2026-07-18 05:04:57 +0000

    feat: Read sample text and print it

--- word_freq_counter/src/main.rs
+++ word_freq_counter/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
+fn read_file(file_path: String) -> String {
+    return fs::read_to_string(file_path).expect("Should have been able to read file");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents : String = read_file("/workspaces/word-frequency-counter/word_freq_counter/src/odyssey_book1.txt".to_string());
+    println!("{}", contents);
 }
