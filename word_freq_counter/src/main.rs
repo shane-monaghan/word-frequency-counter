41a79138f2a2431f89c96134d6e4d6a1128869cf
shane-monaghan <135287482+shane-monaghan@users.noreply.github.com>
2026-07-18 05:26:23 +0000

    feat: Get counts of each word in the text

--- word_freq_counter/src/main.rs
+++ word_freq_counter/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::collections::HashMap;
+use std::str::SplitWhitespace;
 
 fn read_file(file_path: String) -> String {
     return fs::read_to_string(file_path).expect("Should have been able to read file");
@@ -7,4 +9,16 @@ fn read_file(file_path: String) -> String {
 fn main() {
     let contents : String = read_file("/workspaces/word-frequency-counter/word_freq_counter/src/odyssey_book1.txt".to_string());
     println!("{}", contents);
+
+    let mut word_counts: HashMap<String, i32> = HashMap::new();
+    let words: SplitWhitespace<'_> = contents.split_whitespace();
+
+    for word in words {
+        *word_counts.entry(word.to_string()).or_insert(0) += 1;
+    }
+    
+    for k in word_counts.keys() {
+        println!("Count of {} = {}", k.to_string(), word_counts[k].to_string());
+    }
+    
 }
